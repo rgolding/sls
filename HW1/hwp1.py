cf7b13bd1db36622982e21c10ad3b4cd11b5da9e
rgolding <golding.rh@gmail.com>
2018-04-26 17:30:39 -0400

    API key file

--- HW1/hwp1.py
+++ HW1/hwp1.py
@@ -3,22 +3,28 @@ import json
 import time
 import codecs
 import sys
-meetup_api_key = ''
+
 
 
 
 def main():
 
+	key = get_api_key()
+	if (not key):
+		return None
+
+
 	#Get cities to be pulled from Meetup
 	locations = getCitiesFromCSV()
 	print(locations)
+	print("api key:", key)
 
 	for city in locations:
 		per_page = 10
 		current_results = per_page
 		offset = 0
 		while (current_results == per_page):
-			params = {"sign":"true","country":"US", "city":city[0], "state":city[1], "radius": 10, "key":meetup_api_key, "page":per_page, "offset":offset }
+			params = {"sign":"true","country":"US", "city":city[0], "state":city[1], "radius": 10, "key":key, "page":per_page, "offset":offset }
 			response = call(params)
 			time.sleep(1)
 			offset += 1
@@ -47,6 +53,20 @@ def getCitiesFromCSV():
 		locations.append([place[0].strip(), place[1].strip()])
 
 	return locations
+
+def get_api_key():
+	key = ""
+
+	try:
+		f = open('put_your_api_key_here.txt', 'r')
+		key = f.readline().strip()
+		return key
+		
+	except: 
+		print("you forgot your api key.")
+		return None
+
+
 
 
 if __name__=="__main__":
